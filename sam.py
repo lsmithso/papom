9ccde4a57720907557aa0f4c2fc233d7e95a9e72
Les Smithson <lsmithso@hare.demon.co.uk>
2013-02-25 16:43:36 +0000

    Add ControlsMixin class

--- sam.py
+++ sam.py
@@ -33,9 +33,35 @@ class Node(object):
 	self.obj = obj
 
 
-    
 
-class Sink(Node):
+class ControlsMixin(object):
+
+    I_DEVICE = "org.PulseAudio.Core1.Device"
+    
+    def get_volume(self):
+	# Assumes balanced mono
+	    vs = self.obj.Get(self.I_DEVICE, "Volume",  dbus_interface=I_PROP)
+	    return  int(vs[0])
+	    
+
+    def set_volume(self, v):
+	v1 = dbus.UInt32(v)
+	self.obj.Set(self.I_DEVICE, "Volume", [v1, v1], dbus_interface = I_PROP)
+
+    volume = property(get_volume, set_volume)
+
+    def get_mute(self):
+	return 	    self.obj.Get(self.I_DEVICE, "Mute",  dbus_interface=I_PROP)
+
+
+    def set_mute(self, v):
+	self.obj.Set(self.I_DEVICE, "Mute", v, dbus_interface=I_PROP)
+	    
+    mute = property(get_mute, set_mute)
+    
+	
+
+class Sink(Node, ControlsMixin):
 
     I_SINK_PROP =  "org.PulseAudio.Core1.Device"
 
@@ -48,20 +74,9 @@ class Sink(Node):
 	self.index = self.obj.Get(self.I_SINK_PROP, "Index",  dbus_interface=I_PROP)	
 	self.name = self.obj.Get(self.I_SINK_PROP, "Name", dbus_interface=I_PROP)
 	
-    def get_volume(self):
-	pass
-
-    def set_volume(self, v):
-	pass
-
-    def get_mute(self):
-	pass
-
-    def set_mute(self, v):
-	pass
 
     def __str__(self):
-	return '%d: %s' % (self.index, self.name)
+	return 'Sink %d: %s %s %s' % (self.index, self.name, self.volume, self.mute)
 
 class PlaybackStream(object):
     pass
@@ -78,6 +93,8 @@ if __name__ == '__main__':
     Sink.build(conn, core)
     for k, v in Sink.nodes.items():
 	print k, v
-    
-    
-    
+	if v.index == 0:
+	    print v.volume
+	    v.volume = 60000
+	    print v.mute
+	    v.mute = False
