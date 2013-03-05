3942e98a82e0314b04801f789099860df297bdfa
Les Smithson <lsmithso@hare.demon.co.uk>
2013-03-05 17:49:42 +0000

    start packaging

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(name='stfu',
+      version='0.0.1',
+      py_modules=['stfu'],
+      description='Manage pulseaudio sinks, sources and clients in useful ways',
+      author='Les Smithson',
+      author_email='lsmithso@hare.demon.co.uk',
+      url='http://open-networks.co.uk',
+      scripts = ['stfu'],
+      provides=['stfu'],
+
+
+      )
